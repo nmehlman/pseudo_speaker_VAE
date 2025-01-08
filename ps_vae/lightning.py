57fbde04b765220c539602fd9bdd5ac6660e2868
nmehlman <nmehlman@usc.edu>
2025-01-08 10:46:37 -0800

    Reformat of lightning code

--- ps_vae/lightning.py
+++ ps_vae/lightning.py
@@ -3,28 +3,30 @@ import torch.nn as nn
 from model import VAEModel
 import torch
 
-class PseudoSpeakerVAE(pl.LightningModule):
 
+class PseudoSpeakerVAE(pl.LightningModule):
     def __init__(self, **hparams):
         super().__init__()
         self.hparams = hparams
         self.log_hyperparams()
 
-        self.model = VAEModel(**hparams['model'])
-        
-        self.kl_loss_weight = hparams.get('kl_loss_weight', 1.0)      
+        self.model = VAEModel(**hparams["model"])
+
+        self.kl_loss_weight = hparams.get("kl_loss_weight", 1.0)
 
     def forward(self, x):
         x_hat, mu, sigma = self.model(x)
         return x_hat, mu, sigma
-    
+
     def training_step(self, batch: tuple, batch_idx: int) -> float:
 
         x, _ = batch
         x_hat, mu, log_sigma = self(x)
-        
-        mse_loss = nn.functional.mse_loss(x_hat, x, reduction='mean')
-        kl_loss = -0.5 * torch.mean(torch.sum(1 + log_sigma - mu.pow(2) - log_sigma.exp(), dim=-1))
+
+        mse_loss = nn.functional.mse_loss(x_hat, x, reduction="mean")
+        kl_loss = -0.5 * torch.mean(
+            torch.sum(1 + log_sigma - mu.pow(2) - log_sigma.exp(), dim=-1)
+        )
 
         total_loss = mse_loss + self.kl_loss_weight * kl_loss
 
