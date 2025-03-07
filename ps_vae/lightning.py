6b8b5a5296b971712de0cab3b55c74293fb59cb8
Nick Mehlman <nmehlman@usc.edu>
2025-03-06 18:38:52 -0800

    Updated lightning

--- ps_vae/lightning.py
+++ ps_vae/lightning.py
@@ -4,7 +4,7 @@ from ps_vae.model import VAEModel
 from ps_vae.classifier import Classifier
 import torch
 from torch.optim import Adam
-
+from torchmetrics import Accuracy
 
 class PseudoSpeakerVAE(pl.LightningModule):
     def __init__(self, **hparams):
@@ -13,8 +13,21 @@ class PseudoSpeakerVAE(pl.LightningModule):
         self.save_hyperparameters()
 
         self.model = VAEModel(**hparams["model"])
+
+        if hparams.get("vae_checkpoint", None):
+            print(f"Using VAE checkpoint {hparams['vae_checkpoint']}")
+            self.load_vae_from_checkpoint(hparams["vae_checkpoint"])
+
+        if hparams.get("freeze_vae", False):
+            print("Freezing VAE")
+            for param in self.model.parameters():
+                param.requires_grad = False
+
         if 'classifier' in hparams:
             self.classifier = Classifier(**hparams["classifier"])
+            self.accuracy = Accuracy()
+        else:
+            self.classifier = None
         
         self.kl_loss_weight = hparams.get("kl_loss_weight", 1.0)
         self.classifier_loss_weight = hparams.get("classifier_loss_weight", 1.0)
@@ -28,8 +41,14 @@ class PseudoSpeakerVAE(pl.LightningModule):
         x, y = batch
         x_hat, mu, log_sigma = self(x)
 
-        y_hat = self.classifier(mu)
-        classifier_loss = nn.functional.cross_entropy(y_hat, y)
+        if self.classifier:
+            y_hat = self.classifier(mu)
+            classifier_loss = nn.functional.cross_entropy(y_hat, y)
+            classifier_acc = self.accuracy(y_hat, y)
+            self.log("train_classifier_acc", classifier_acc)
+            self.log("train_classifier_loss", classifier_loss)
+        else:
+            classifier_loss = 0
 
         mse_loss = nn.functional.mse_loss(x_hat, x, reduction="mean")
         kl_loss = -0.5 * torch.mean(
@@ -46,7 +65,6 @@ class PseudoSpeakerVAE(pl.LightningModule):
         self.log("train_loss", total_loss)
         self.log("train_mse_loss", mse_loss)
         self.log("train_kl_loss", kl_loss)
-        self.log("train_classifier_loss", classifier_loss)
 
         return {"loss": total_loss}
     
@@ -55,8 +73,14 @@ class PseudoSpeakerVAE(pl.LightningModule):
         x, y = batch
         x_hat, mu, log_sigma = self(x)
 
-        y_hat = self.classifier(mu)
-        classifier_loss = nn.functional.cross_entropy(y_hat, y)
+        if self.classifier:
+            y_hat = self.classifier(mu)
+            classifier_loss = nn.functional.cross_entropy(y_hat, y)
+            classifier_acc = self.accuracy(y_hat, y)
+            self.log("train_classifier_acc", classifier_acc)
+            self.log("val_classifier_loss", classifier_loss, sync_dist=True, batch_size=x.size(0))
+        else:
+            classifier_loss = 0
 
         mse_loss = nn.functional.mse_loss(x_hat, x, reduction="mean")
         kl_loss = -0.5 * torch.mean(
@@ -73,9 +97,13 @@ class PseudoSpeakerVAE(pl.LightningModule):
         self.log("val_loss", total_loss, sync_dist=True, batch_size=x.size(0))
         self.log("val_mse_loss", mse_loss, sync_dist=True, batch_size=x.size(0))
         self.log("val_kl_loss", kl_loss, sync_dist=True, batch_size=x.size(0))
-        self.log("val_classifier_loss", classifier_loss, sync_dist=True, batch_size=x.size(0))
 
         return {"loss": total_loss}
+
+    def load_vae_from_checkpoint(self, checkpoint_path):
+        checkpoint = torch.load(checkpoint_path, map_location=self.device)
+        vae_state_dict = {k.replace("model.", ""): v for k, v in checkpoint["state_dict"].items() if k.startswith("model.")}
+        self.model.load_state_dict(vae_state_dict)
 
     def configure_optimizers(self):
         optimizer = Adam(self.model.parameters(), **self.hparams["optimizer"])
