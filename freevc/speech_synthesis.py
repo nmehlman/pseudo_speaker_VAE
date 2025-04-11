2134aa1a5b5dbba39d7bad965fc34f66ffcd6af9
Nick Mehlman <nmehlman@usc.edu>
2025-04-11 15:12:49 -0700

    Gender trasnform code

--- freevc/speech_synthesis.py
+++ freevc/speech_synthesis.py
@@ -2,8 +2,10 @@ import torch
 import torchaudio
 import os
 import sys
-
+import certifi
 from utils import load_freevc_model
+
+os.environ["SSL_CERT_FILE"] = certifi.where()  # Set SSL_CERT_FILE for TTS
 
 FREE_VC_INPUT_SR = 16000
 FREE_VC_OUTPUT_SR = 24000
@@ -44,18 +46,20 @@ if __name__ == "__main__":
     embedding_dir = args.embedding_dir
     save_dir = args.save_dir
     vc_ckpt_dir = args.vc_ckpt_dir
+    
+    os.makedirs(save_dir, exist_ok=True)
 
     # Load the FreeVC model
     model = load_freevc_model(vc_ckpt_dir)
 
     for embed_file in tqdm.tqdm(os.listdir(embedding_dir), desc="Converting audio"):
         
-        if not embed_file.endswith(".pt"):      
+        if not embed_file.endswith(".pt"): 
             continue
         
-        id = embed_file.split("_")[-1].replace(".pt", "")
+        id = embed_file.replace(".pt", "") 
         
         emedding = torch.load(os.path.join(embedding_dir, embed_file)).reshape(1,-1,1)
         converted_audio = run_embedding_conditioned_vc(emedding, source_audio_path, free_vc_model=model)
-        torchaudio.save(os.path.join(save_dir, f"sample_embed_{id}.wav"), converted_audio, FREE_VC_OUTPUT_SR)
+        torchaudio.save(os.path.join(save_dir, f"audio_{id}.wav"), converted_audio, FREE_VC_OUTPUT_SR)
         
